6f307fa3a83112f73b19183d49a8b2ff37a9abf1
Tosin Omotayo <oluwaetosin@gmail.com>
2024-08-11 16:43:08 +0100

    style the active link in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,13 @@
-import { link } from 'fs';
+'use client';
+import classNames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiOutlineBug } from "react-icons/ai";
 
 const NavBar = () => {
+    const path = usePathname();
+
     const navItems = [
         {
             link: '/',
@@ -14,13 +18,18 @@ const NavBar = () => {
             text: 'Issues'
         }
     ]
+
     return (
         <nav className='flex space-x-6 border-b px-5 h-20 mb-5 items-center text-2xl'>
             <Link href="/"><AiOutlineBug /></Link>
             <ul className='flex space-x-6'>
                 {
                     navItems.map(item =>
-                        <li className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+                        <li className={classNames({
+                            'text-zinc-900': path === item.link,
+                            'text-zinc-500': path !== item.link,
+                            'hover:text-zinc-800': true
+                        })}>
                             <Link key={item.text} href={item.link}>{item.text}</Link>
                         </li>
                     )
